55e94018b3e4019203e3c964be2b1453fc465f7d
korhlibri <hlibk@proton.me>
2024-09-17 18:34:03 -0230

    Implement delay option, comment code

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::sync::{Arc, Mutex};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::thread;
-use std::time::Instant;
+use std::time;
 
-/// Make an HTTP request and receive data characteristics
+/// Make an HTTP request repeatedly with a wordlist and receive data characteristics
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -63,28 +63,45 @@ struct Args {
     /// 
     /// Example: -u "http://example.com" -t 4
     #[arg(short, long, default_value_t = 1)]
-    threads: u32,
+    threads: u16,
+
+    /// Amount of delay in milliseconds between each request. Multiplied by amount of threads.
+    /// 
+    /// Example: -u "http://example.com" -d 100
+    #[arg(short, long)]
+    delay: Option<u16>,
 }
 
 fn main() {
-    let http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE"];
     let args = Args::parse();
-    let wordlist = Arc::new(Mutex::new(Vec::<String>::new()));
-    let filler = Arc::clone(&wordlist);
 
-    load_words_to_memory(args.list, filler);
+    // List of allowed methods to verify user input.
+    let http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE"];
+    // This vec will contain all of the words from the wordlist.
+    let wordlist = Arc::new(Mutex::new(Vec::<String>::new()));
+
+    load_words_to_memory(args.list, Arc::clone(&wordlist));
 
     if !http_methods.contains(&args.method.as_str()) {
         panic!("Method not valid")
     }
 
     let mut handles = Vec::new();
-    let now = Instant::now();
+    let now = time::Instant::now();
     for _ in 0..args.threads {
         let wordlist = Arc::clone(&wordlist);
         let handle = thread::spawn(move || {
+            // Arguments are parsed again due to the move keyword.
+            // This allows us to reuse the arguments every time a new thread is created without the
+            // problem of ownership.
             let args = Args::parse();
+
             let mut headers: Vec<Vec<(String, Vec<(usize, _)>)>> = Vec::new();
+
+            // This loop parses all headers, splits them into key and value, and detects
+            // delimiters without replacing them.
+            // Headers need to be split into key and value to pass them to the reqwest library.
+            // This also verifies that the headers are valid by splitting them into 2 parts.
             for header in &args.header {
                 let splitheader: Vec<&str> = header.split(": ").collect();
                 if splitheader.len() != 2 {
@@ -105,6 +122,7 @@ fn main() {
                 headers.push(tempvec);
             }
         
+            // Parses the body, detecting the delimiters, same as the headers loop.
             let mut bodies: Option<(String, Vec<(usize, _)>)> = None;
             if let Some(body) = &args.body {
                 let indices: Vec<(usize, _)> = body.match_indices(args.delim.as_str()).collect();
@@ -116,6 +134,10 @@ fn main() {
                 bodies = Some((body.clone(), indices));
             }
 
+            // We need to create a client to disallow redirects. By default, reqwest follows all
+            // redirects. This is detrimental depending on the performed activity, but by creating
+            // a client there is extra overhead in performance.
+            // We use blocking because of multithreading. By default, the library uses async tasks.
             let mut client = reqwest::blocking::ClientBuilder::new();
             if !args.allowredirects {
                 client = client.redirect(reqwest::redirect::Policy::none());
@@ -123,6 +145,8 @@ fn main() {
             let clientready = client.build().unwrap();
 
             loop {
+                // This segment of code gets the vec of words, takes a word, and unlocks the vec.
+                // This allows for the vec to be freed for other threads to use it immediately.
                 let mut wordsmutex = wordlist.lock().unwrap();
                 let word = match wordsmutex.pop() {
                     Some(word) => word,
@@ -130,14 +154,26 @@ fn main() {
                 };
                 drop(wordsmutex);
 
-                let mut req = clientready.request(reqwest::Method::from_bytes(args.method.as_bytes()).unwrap(), args.url.as_str());
+                let mut req = clientready.request(
+                    reqwest::Method::from_bytes(args.method.as_bytes()).unwrap(),
+                     args.url.as_str()
+                );
 
+                // This loop is in charge of replacing the delimiters with the word from the
+                // wordlist. We use the vec of already detected delimiters to facilitate it.
                 for header in &headers {
                     let mut iterator: usize = 0;
                     let mut key: String = String::from("");
                     let mut value: String = String::from("");
+                    // Checks if any delimiters were detected in the header key. If not, pushes the
+                    // header as is.
                     if header[0].1.len() > 0 {
+                        // Iterates each delimiter detected in pairs (2 delimiters need to surround
+                        // the place where the word will go from the wordlist).
                         while iterator < header[0].1.len() {
+                            // first_delim_pos = left delimiter which corresponds to the beginning
+                            // of the word.
+                            // last_delim_pos = last delimiter before the left most delimiter.
                             let first_delim_pos = header[0].1[iterator].0;
                             let last_delim_pos = {
                                 if iterator == 0 {
@@ -152,6 +188,8 @@ fn main() {
                             iterator += 2;
                         }
 
+                        // Once we reach the end, we push to the string anything that is after the
+                        // last delimiter inside the string (in this case the header key).
                         let last_delim_pos = header[0].1[iterator-1].0+args.delim.len();
                         if last_delim_pos < header[0].0.len() {
                             key.push_str(&header[0].0[last_delim_pos..])
@@ -161,6 +199,8 @@ fn main() {
                         key.push_str(&header[0].0);
                     }
                     iterator = 0;
+                    // Works identically to the last iteration, but this time, checks the header
+                    // value instead.
                     if header[1].1.len() > 0 {
                         while iterator < header[1].1.len() {
                             let first_delim_pos = header[1].1[iterator].0;
@@ -184,9 +224,12 @@ fn main() {
                     } else {
                         value.push_str(&header[1].0);
                     }
+                    // Once everything is verified, pushes the key and value of the header.
                     req = req.header(&key, &value);
                 }
 
+                // Works similarly to the header verification of delimiters, without the need to
+                // verify keys or values, as body only has a single string instance.
                 if let Some(body) = &bodies {
                     let mut value: String = String::from("");
                     let mut iterator = 0;
@@ -213,9 +256,9 @@ fn main() {
                     } else {
                         value.push_str(&body.0);
                     }
-                    println!("{}", value);
                     req = req.body(value);
                 }
+                // Sends the response, blocking the thread until receiving a reply.
                 let resp = req.send().unwrap();
 
                 let status = resp.status();
@@ -225,6 +268,12 @@ fn main() {
                 println!("Status code: {}. Length: {}. Word: {}", status, text.len(), word);
                 if args.verbose {
                     println!("{:#?}\n{:#}", resp_headers, text);
+                }
+                // Delays the thread by amount equal to in threads. Multiplies the delay by the
+                // amount of threads to account for batch requests that threads make
+                // simultaneously.
+                if let Some(delay) = &args.delay {
+                    thread::sleep(time::Duration::from_millis((delay * args.threads) as u64));
                 }
             }
         });
@@ -238,9 +287,13 @@ fn main() {
     println!("Complete! Time taken: {:.2?}", elapsed);
 }
 
+// Reads all words from a file and pushes them to the Vec in Arc Mutex. Allows for easier access
+// later in the program.
 fn load_words_to_memory(filename: String, wordlist: Arc<Mutex<Vec<String>>>) {
     let mut vec = wordlist.lock().unwrap();
-    let line_iterator = io::BufReader::new(File::open(filename).unwrap()).lines();
+    let line_iterator = io::BufReader::new(
+        File::open(filename).unwrap()
+    ).lines();
 
     for line in line_iterator {
         vec.push(line.unwrap());
